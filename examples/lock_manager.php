398d24289647d962797bfab6bccdb9ffce95096a
Hett <hettbox@gmail.com>
2015-10-05 13:25:08 +0600

    FIXES for php client

--- examples/lock_manager.php
+++ examples/lock_manager.php
@@ -1,47 +1,53 @@
 <?php
 
-class LockManager {
+class LockManager
+{
 
-    public $hostname = "tcp://127.0.0.1";
+    public $server = 'tcp://127.0.0.1';
+    public $connectTimeout = 0.2;
 
     protected $_buffer;
     protected $_socket;
 
-    public function __construct($connectTimeout = 0.2)
+    public function init()
     {
-        $this->_socket = @fsockopen($this->hostname, 1234, $errno, $errstr, $connectTimeout);
-        if($this->_socket === false) {
-            //@todo error logging
+        $this->_socket = @fsockopen($this->server, 1234, $errno, $errstr, $this->connectTimeout);
+        if ($this->_socket === false) {
+            //@todo logging
         }
     }
 
-    protected function read_line() {
+    protected function read_line()
+    {
 
         while (strpos($this->_buffer, "\n") === false)
             $this->_buffer .= fread($this->_socket, 64);
 
         $lineEnd = strpos($this->_buffer, "\n");
         $line = substr($this->_buffer, 0, $lineEnd);
-        $this->_buffer = substr($this->_buffer, $lineEnd);
+
+        $this->_buffer = substr($this->_buffer, $lineEnd + 1);
 
         return $line;
     }
 
-    protected function write_line($line) {
+    protected function write_line($line)
+    {
         fwrite($this->_socket, $line . "\n");
     }
 
     public function acquire($name, $timeoutMs = 0)
     {
-        if($this->_socket === false) {
+        if ($this->_socket === false) {
             return true;
         }
         $this->write_line("LOCK {$name} {$timeoutMs}");
         $response = $this->read_line();
-        switch($response) {
-            case "SUCCESS":
+
+        switch ($response) {
+            case 'SUCCESS':
                 return true;
-            case "BUSY":
+            case 'BUSY':
                 return false;
             default:
                 //@todo log unknown response
@@ -51,7 +57,7 @@ class LockManager {
 
     public function release($name)
     {
-        if($this->_socket === false) {
+        if ($this->_socket === false) {
             return;
         }
 
